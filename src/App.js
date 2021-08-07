72bb1023b4a1ecf0b9754934d85e0a95c427a7d6
Spencer Saunders <spencer.saunders45@gmail.com>
2021-08-07 10:00:06 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './views/Home/Home';
+import About from './views/About/About';
+import Projects from './views/Projects/Projects';
+import { Router } from '@reach/router';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Home path='/' />
+        <About path='/about' />
+        <Projects path='projects' />
+      </Router>
     </div>
   );
 }
