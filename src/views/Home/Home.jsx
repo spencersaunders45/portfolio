2b3ff6b8644adcbbbf3de046290e49e4a446e7f6
Spencer Saunders <spencer.saunders45@gmail.com>
2022-02-25 12:17:14 -0600

    fixed for mobile

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Home.css';
 import Me from '../../images/profile-pic.jpg';
@@ -8,18 +8,18 @@ import GitHub from '../../images/GitHub.png';
 
 export default () => {
   return(
-    <div className='container-fluid d-flex align-items-center justify-content-center'>
-      <div className='row d-flex jstify-content-center' id="main">
-        <div className='col-1 link-side d-flex flex-column align-items-center justify-content-center'>
+    <div className='container-fluid d-flex justify-content-center align-items-center'>
+      <div id="main">
+        <div className='link-side d-flex flex-column align-items-center justify-content-center'>
           <img id='profile-pic' src={Me} alt="White male with beard in blue suite with red floaral tie and glasses" />
-          <h2>Spencer Saunders</h2>
-          <div id='social-links'>
+          <h2 id='name'>Spencer Saunders</h2>
+          <div id='social-links d-flex'>
             <a href="https://twitter.com/SpencerS45"><img className='social-img' src={Twitter} alt="Twitter Logo" /></a>
             <a href="https://github.com/spencersaunders45"><img className='social-img' src={GitHub} alt="GitHub Logo" /></a>
             <a href="https://www.linkedin.com/in/spencer-saunders-developer/"><img className='social-img' src={LinkedIn} alt="LinkedIn Logo" /></a>
           </div>
         </div>
-        <div className='col info-side py-5'>
+        <div className='info-side border border-white'>
           <img id='pixel-dude' src="https://media.giphy.com/media/5eLDrEaRGHegx2FeF2/giphy.gif" alt="pixel dude typing" />
           <h3 className='border-bottom border-white p-3 mb-3'>Software Developer</h3>
           <div>
@@ -31,7 +31,7 @@ export default () => {
         </div>
       </div>
       <footer>
-        <a id='freepik' className='fixed-bottom d-flex justify-content-start' href='https://www.freepik.com/vectors/background'>Background vector created by freepik - www.freepik.com</a>
+        <a id='freepik' className='fixed-bottom justify-content-start' href='https://www.freepik.com/vectors/background'>Background vector created by freepik - www.freepik.com</a>
       </footer>
     </div>
   )
